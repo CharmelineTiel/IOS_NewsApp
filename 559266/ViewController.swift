0d775a922d19b032521515f3c90691e64adb7bba
Steven Buteneers <steven_buteneers@live.nl>
2017-10-24 12:59:01 +0200

    Initial Commit

---
+++ 559266/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  559266
+//
+//  Created by Inholland Haarlem on 24-10-17.
+//  Copyright © 2017 CharmelineTiel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
